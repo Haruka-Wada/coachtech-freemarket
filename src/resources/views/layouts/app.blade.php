067938944d6fe29a79d08b4ac30c86be405a052d
haruka wada <022.wadaha@gmail.com>
2025-01-02 01:08:47 +0900

    sellページのcss修正

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
     <link rel="stylesheet" href="{{ asset('css/layouts/common.css') }}">
     @yield('css')
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 </head>
 
 <body>
--- src/resources/views/sell.blade.php
+++ src/resources/views/sell.blade.php
@@ -9,62 +9,73 @@
     <div class="main__title">
         <h2>商品の出品</h2>
     </div>
-    <div class="item__contents">
-        <div class="item__label">
-            <p>商品画像</p>
+    <form action="/sell" method="post" enctype="multipart/form-data">
+        @csrf
+        <div class="item__contents">
+            <div class="item__label">
+                <p>商品画像</p>
+            </div>
+            <div class="item__data-file">
+                <button type="button" for="upload" class="upload__button">画像を選択する</button>
+                <input type="file" id="upload" class="item__data-upload">
+            </div>
         </div>
-        <div class="item__data">
-            <input type="file">
+        <div class="item__contents">
+            <div class="item__headline">
+                <p>商品の詳細</p>
+            </div>
+            <div class="item__label">
+                <p>カテゴリー</p>
+            </div>
+            <div class="item__data">
+            </div>
+            <div class="item__label">
+                <p>商品の状態</p>
+            </div>
+            <div class="item__data">
+                <select name="condition" id="condition">
+                    <option value="" selected hidden>選択してください</option>
+                    @foreach($conditions as $condition)
+                    <option value="{{ $condition->id }}">{{ $condition->name }}</option>
+                    @endforeach
+                </select>
+            </div>
         </div>
-    </div>
-    <div class="item__contents">
-        <div class="item__headline">
-            <p>商品の詳細</p>
+        <div class="item__contents">
+            <div class="item__headline">
+                <p>商品名と説明</p>
+            </div>
+            <div class="item__label">
+                <p>商品名</p>
+            </div>
+            <div class="item__data">
+                <input type="text">
+            </div>
+            <div class="item__label">
+                <p>商品の説明</p>
+            </div>
+            <div class="item__data">
+                <textarea name="description" id=""></textarea>
+            </div>
         </div>
-        <div class="item__label">
-            <p>カテゴリー</p>
+        <div class="item__contents">
+            <div class="item__headline">
+                <p>販売価格</p>
+            </div>
+            <div class="item__label">
+                <p>販売価格</p>
+            </div>
+            <div class="item__data">
+                <input type="text">
+            </div>
         </div>
-        <div class="item__data">
-        </div>
-        <div class="item__label">
-            <p>商品の状態</p>
-        </div>
-        <div class="item__data">
-            <select name="condition" id="condition">
-                <option value="" selected hidden>選択してください</option>
-                @foreach($conditions as $condition)
-                <option value="{{ $condition->id }}">{{ $condition->name }}</option>
-                @endforeach
-            </select>
-        </div>
-    </div>
-    <div class="item__contents">
-        <div class="item__headline">
-            <p>商品名と説明</p>
-        </div>
-        <div class="item__label">
-            <p>商品名</p>
-        </div>
-        <div class="item__data">
-            <input type="text">
-        </div>
-        <div class="item__label">
-            <p>商品の説明</p>
-        </div>
-        <div class="item__data">
-            <textarea name="description" id=""></textarea>
-        </div>
-    </div>
-    <div class="item__contents">
-        <div class="item__headline">
-            <p>販売価格</p>
-        </div>
-        <div class="item__label">
-            <p>販売価格</p>
-        </div>
-        <div class="item__data">
-            <input type="text">
-        </div>
-    </div>
+    </form>
 </div>
+
+<script type="text/javascript">
+    document.querySelector(".upload__button").addEventListener("click", () => {
+        document.querySelector(".item__data-upload").click();
+    });
+</script>
+
 @endsection
